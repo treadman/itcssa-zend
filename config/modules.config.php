36bb96ef651b118d0a4f40b851feb52b7ec2584d
treadman <treadmen@hotmail.com>
2017-02-28 21:28:27 -0500

    added MVC plugins to composer.json

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,6 +11,10 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\Mvc\Plugin\FilePrg',
+    'Zend\Mvc\Plugin\FlashMessenger',
+    'Zend\Mvc\Plugin\Identity',
+    'Zend\Mvc\Plugin\Prg',
     'Zend\Session',
     'Zend\Form',
     'Zend\Db',
